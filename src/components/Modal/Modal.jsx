88db5d60a6fcc6e6b7781491a9a2984ac6b6f564
MariiaHula <mariyagulaya@gmail.com>
2023-11-09 13:48:13 +0200

    added reactDOM portal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,16 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ModalContainer, ModalContent } from './Modal.styled';
 
+const rootModal = document.querySelector('#modal');
+
 const Modal = ({ children }) => {
-  return (
+  return ReactDOM.createPortal(
     <ModalContainer>
       <ModalContent>{children}</ModalContent>
-    </ModalContainer>
+    </ModalContainer>,
+    rootModal
   );
 };
 
